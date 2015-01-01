232cda5641f905fd76bf5b66d7b588f04749e109
ficapy <c4d@outlook.com>
2015-01-01 22:27:28 +0800

    太穷，设置每日可充值金额为2元，同时修改了若干bug

--- recharge.py
+++ recharge.py
@@ -1,39 +1,47 @@
 __author__ = 'LD'
 #coding:utf-8
+#Create on:2015.1.1
+#Version:0.0.2
+
+"""
+某日答应某人每天给冲1元话费，于是就写了这个0_0
+流程为检测可充值金额及余额，如若余额大于金额则提交订单充值获取订单号
+由订单号查询充值状态，若首次检查非失败状态则认为充值成功，若失败则发送
+短信给俺
+"""
 
 import requests
 from hashlib import md5
 import time
 import random
 import sys
-import unittest
 
 ###huafeiduo API
-API_KEY = "--------------------------"
-SECRET_KEY = "----------------------------"
-telephone_number = "--------------------"
+API_KEY = "---------------------------------------"
+SECRET_KEY = "------------------------------------"
+telephone_number = "------------"
 
 ###nexmo API
-KEY = "---------------"
-SECERT = "--------------------"
-my_phone_number = "----------------"
+KEY = "------------------------"
+SECERT = "---------------------"
+my_phone_number = "------------"  ###注意此处需要添加国际区号，但不用添加"+"号
 
 
-class recharge(object):
+class ReCharge(object):
     def __init__(self, money=(1, 2, 5, 10, 20, 50, 100, 300)):
         self.money = money
-        pass
+        self.session = requests.Session()
 
     def sms_notify(self, message):
         """
         充值失败则使用nexmo进行短信提醒
         """
         baseurl = "https://rest.nexmo.com/sms/json"
-        balance = requests.get("https://rest.nexmo.com/account/get-balance", params={
+        balance = self.session.get("https://rest.nexmo.com/account/get-balance", params={
             "api_key": KEY,
             "api_secret": SECERT,
         }).json()["value"]
-        requests.get(baseurl, params={
+        self.session.get(baseurl, params={
             "api_key": KEY,
             "api_secret": SECERT,
             "from": "xxxx",
@@ -50,7 +58,7 @@ class recharge(object):
         #签名算法
         #传入参数字典→排序→键值对相加组成字符串→加上安全码→进行MD5运算
         kwargs.update({'api_key': API_KEY})
-        sign = md5(''.join(sorted([''.join(i) for i in kwargs.iteritems()])) + SECRET_KEY).hexdigest()
+        sign = md5(''.join(sorted(''.join(i) for i in kwargs.iteritems())) + SECRET_KEY).hexdigest()
         params = {
             "mod": rechargetype,
             "sign": sign,
@@ -60,11 +68,10 @@ class recharge(object):
         i = 0
         while i < retry:
             try:
-                info = requests.get(baseurl, params=params, timeout=timeout)
+                info = self.session.get(baseurl, params=params, timeout=timeout)
                 if info.status_code == 200:
                     if rechargetype == "order.phone.submit":     #简直是坑爹的特例
                         return info.json()["order_id"] if info.json()["status"] == "success" else False
-                    print info.json()
                     return info.json()["data"] if info.json()["status"] == "success" else False
             except:
                 pass
@@ -87,51 +94,68 @@ class recharge(object):
         return False
 
     def submit_time(self):
-        hour = random.randint(8, 22)
+        hour = random.randint(8, 19)
         minute = random.randint(0, 60)
         return hour, minute
 
-    def submit(self, telephone_number=telephone_number):
-        money = self.optional_money()
-        print money, type(money)
+    def submit(self, telephone_number=telephone_number, limit_money=2):
+        """
+        对不起啊，我是个穷人，限定到有2元可以充值的时候充值
+        更具下面随机生成的时间最晚是晚上7点，保险起见设置4个小时内查询是否有满足条件的金额
+        所以最多使用4*3600/10次查询,如果运气不好...那么听天由命吧
+        如果充值超过限值，那么未来几天就暂停充值一段时间
+        """
+        i = 0
+        while i < 4 * 3600 / 60:
+            money = self.optional_money()
+            if money <= limit_money:
+                break
+            time.sleep(60)
+
         if self.check_balance() < money + 0.5:
-            self.sms_notify("huafeiduo余额不足,请充值")
+            self.sms_notify("huafeiduo余额不足,速速充值,该脚本会退出你丫赶紧过来重启")
             sys.exit()
         order_id = self.send_request("order.phone.submit",
                                      card_worth=money,
                                      phone_number=telephone_number,
                                      sp_order_id=''.join([str(random.randint(0, 9)) for k in range(15)])
         )
-        print u"在{}尝试给{)充值{}元".format(time.strftime("%H:%M", time.gmtime(time.time() + 8 * 3600)),
+        print u"在{}尝试给{}充值{}元".format(time.strftime("%H:%M", time.gmtime(time.time() + 8 * 3600)),
                                       telephone_number,
                                       money
         )
-        return order_id
+        return order_id, money 
 
     def check_order(self, order_id):
         #仅捕捉充值出错的情况发送短信提醒
         order_info = self.send_request("order.phone.get", order_id=order_id)
         if not order_id:
-            self.sms_notify("我艹，为何充值会出我我真不造")
+            self.sms_notify("我艹，为何充值会出错我真不造")
 
 
-def main(recharge=recharge):
+def main(recharge=ReCharge):
     recharge = recharge()
     flag = int(time.strftime("%d", time.gmtime(time.time() + 8 * 3600)))
-    hour, minute = recharge.submit_time()
+    hour, minute = 0, 0   #首次测试用，可同时设置limit_money为10
+    # hour, minute = recharge.submit_time()
     while True:
         time.sleep(10)
-        nowday = int(time.strftime("%d", time.gmtime(time.time() + 8 * 3600)))
+        nowday = int(time.strftime("%d", time.gmtime(time.time() + 8 * 3600)))，
         now_hour, now_minute = time.strftime("%H %M", time.gmtime(time.time() + 8 * 3600)).split()
         if flag == nowday:
             if int(now_hour) + int(now_minute) / 60.0 >= hour + minute / 60.0:
-                order_id = recharge.submit()
+                order_id, money = recharge.submit()
+                #如果失败最大的可能为查询是可以，但是充值金额不满足，此情况重新循环
                 if not order_id:
-                    recharge.sms_notify("huafeiduo余额充足但是莫名其妙的充值失败，请手动检查")
+                    print u'充值失败，再来一次'
                 else:
                     recharge.check_order(order_id)
-                flag = nowday + 1
-                hour, minute = recharge.submit_time()
+                    if money > 2:
+                        flag = int(time.strftime("%d", time.gmtime(epoch + (money - 2) / 2 * 24 * 3600)))
+                    else:
+                        # 日期推后一天
+                        flag = int(time.strftime("%d", time.gmtime(epoch+24*3600)))
+                    hour, minute = recharge.submit_time()
 
 
 if __name__ == "__main__":
