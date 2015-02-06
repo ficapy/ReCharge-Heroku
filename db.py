f4f9804e108a84a054aadd4a747794ff3ad0acb5
ficapy <c4d@outlook.com>
2015-02-07 00:43:57 +0800

    修改gitignore文件

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#Create on:2015.1.19
+#Create on:2015.1.3
+#Version:0.0.1
 
 """
 因为heroku无法进行本地存储和读取，所有选择用数据库存储记录数据
@@ -9,70 +10,36 @@
 
 import os
 import urlparse
-from sqlalchemy import Column, DateTime, Integer, create_engine,Boolean,String
+from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime
 
 Base = declarative_base()
 
-class TimeSign(Base):
-    __tablename__ = 'time_sign'
-    id = Column(Integer, primary_key=True)
-    ts = Column(DateTime, default=datetime.utcnow())
-
-    @staticmethod
-    def gettime():
-        return s.query(TimeSign).first().ts
-
-
-    @staticmethod
-    def settime(time=datetime.utcnow()):
-        s.query(TimeSign).update({"ts": time})
-        s.commit()
-
-
-    @staticmethod
-    def fight():
-        #datetime.datetime.now()>datetime.datetime.utcnow() + datetime.timedelta(hours=1)→True
-        #统一使用UTC
-        if datetime.utcnow() > TimeSign.gettime() and 0 <= datetime.utcnow().hour <= 9:
-            return True
-        return False
 
 class ReChargeLog(Base):
     __tablename__ = 'ReChargeLog'
     id = Column(Integer, primary_key=True)
     msg = Column(String)
 
-    @staticmethod
-    def savemsg(message):
-        d = ReChargeLog(msg=message)
-        s.add(d)
-        s.commit()
+    def __init__(self, msg):
+        self.msg = msg
 
 
-#方便本地测试
-if "DATABASE_URL" in list(os.environ):
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
-    engine = create_engine('postgresql+psycopg2://{user}:{pwd}@{host}:{port}/{database}'.format(database=url.path[1:],
-                                                                                                user=url.username,
-                                                                                                pwd=url.password,
-                                                                                                host=url.hostname,
-                                                                                                port=url.port))
-else:
-    engine = create_engine('postgresql+psycopg2://{user}:{pwd}@{host}:{port}/{database}'.format(database="test",
-                                                                                                user="test_postgres",
-                                                                                                pwd="123456",
-                                                                                                host="localhost",
-                                                                                                port=5432))
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+engine = create_engine('postgresql+psycopg2://{user}:{pwd}@{host}:{port}/{database}'.format(database=url.path[1:],
+                                                                                            user=url.username,
+                                                                                            pwd=url.password,
+                                                                                            host=url.hostname,
+                                                                                            port=url.port))
 session = sessionmaker()
 session.configure(bind=engine)
 Base.metadata.create_all(engine)
 s = session()
-#设置初始时间
-if not s.query(TimeSign).first():
-    inittime = TimeSign()
-    s.add(inittime)
+
+
+def msg(msg):
+    d = ReChargeLog(msg)
+    s.add(d)
     s.commit()
